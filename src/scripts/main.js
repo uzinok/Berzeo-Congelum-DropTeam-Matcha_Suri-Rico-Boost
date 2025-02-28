b36341a5427c1dbfdbbd503fb48ef63fc45dfdba
uzinok <41639106+uzinok@users.noreply.github.com>
2025-02-28 17:18:30 +0300

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,14 +8,43 @@ document.querySelectorAll('a[href^="#"]:not(a[href="#"])').forEach(anchor => {
 	});
 });
 
+const heroSwiper = new Swiper('.hero__swiper', {
+	speed: 400,
+	spaceBetween: 14,
+	navigation: {
+		nextEl: '.hero__navigation-prev',
+		prevEl: '.hero__navigation-next',
+	},
+
+	breakpoints: {
+		768: {
+			slidesPerView: 2,
+		},
+		990: {
+			slidesPerView: 1,
+			spaceBetween: 23,
+		}
+	}
+});
 
 const naturalSwiper = new Swiper('.natural__swiper', {
 	speed: 400,
 	spaceBetween: 14,
+	navigation: {
+		nextEl: '.natural__navigation-prev',
+		prevEl: '.natural__navigation-next',
+	},
 
 	breakpoints: {
+		768: {
+			slidesPerView: 2,
+		},
 		990: {
+			slidesPerView: 3,
 			spaceBetween: 23,
+		},
+		1253: {
+			slidesPerView: 1
 		}
 	}
 });
@@ -23,10 +52,21 @@ const naturalSwiper = new Swiper('.natural__swiper', {
 const standartsSwiper = new Swiper('.standarts__swiper', {
 	speed: 400,
 	spaceBetween: 10,
+	navigation: {
+		nextEl: '.standards__navigation-prev',
+		prevEl: '.standards__navigation-next',
+	},
 
 	breakpoints: {
+		600: {
+			slidesPerView: 2,
+		},
+		850: {
+			slidesPerView: 3,
+		},
 		989: {
 			slidesPerView: 333,
+			slidesPerView: 4
 		}
 	}
 });
@@ -34,10 +74,21 @@ const standartsSwiper = new Swiper('.standarts__swiper', {
 const componentsSwiper = new Swiper('.components__swiper', {
 	speed: 400,
 	spaceBetween: 24,
+	navigation: {
+		nextEl: '.components__navigation-prev',
+		prevEl: '.components__navigation-next',
+	},
 
 	breakpoints: {
+		768: {
+			slidesPerView: 2,
+		},
 		990: {
+			slidesPerView: 3,
 			spaceBetween: 45,
+		},
+		1253: {
+			slidesPerView: 1
 		}
 	}
 });
@@ -45,10 +96,21 @@ const componentsSwiper = new Swiper('.components__swiper', {
 const reviewSwiper = new Swiper('.review__swiper', {
 	speed: 400,
 	spaceBetween: 10,
+	navigation: {
+		nextEl: '.review__navigation-prev',
+		prevEl: '.review__navigation-next',
+	},
 
 	breakpoints: {
+		768: {
+			slidesPerView: 2,
+		},
 		990: {
+			slidesPerView: 3,
 			spaceBetween: 44,
+		},
+		1253: {
+			slidesPerView: 1
 		}
 	}
 });
@@ -56,10 +118,21 @@ const reviewSwiper = new Swiper('.review__swiper', {
 const topProductsSwiper = new Swiper('.top-products__swiper', {
 	speed: 400,
 	spaceBetween: 18,
+	navigation: {
+		nextEl: '.top-products__navigation-prev',
+		prevEl: '.top-products__navigation-next',
+	},
 
 	breakpoints: {
+		768: {
+			slidesPerView: 2,
+		},
 		990: {
+			slidesPerView: 3,
 			spaceBetween: 14,
+		},
+		1253: {
+			slidesPerView: 4
 		}
 	}
 });
